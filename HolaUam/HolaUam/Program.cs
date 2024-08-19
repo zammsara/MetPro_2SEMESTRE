b2381d21074c42b96f5fb7783f86036be24a3c48
zammsara <saztaylor@uamv.edu.ni>
2024-08-19 08:05:12 -0600

    S1.19AGOS

---
+++ HolaUam/HolaUam/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HolaUam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola UAM");
+            Console.WriteLine("Mi nombre es Sara");
+        }
+    }
+}
