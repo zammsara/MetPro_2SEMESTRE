8807495e144182025f0d30595686b3f3a0147603
zammsara <saztaylor@uamv.edu.ni>
2024-08-23 08:45:01 -0600

    23 AGO

---
+++ SEMANA 1/Compras/Compras/Program.cs
@@ -0,0 +1,33 @@
+﻿using Compras.clases;
+
+namespace Compras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo 1");
+            Factura fact = new Factura();
+            string producto = null;
+            double precio=0, monto;
+            int cantidad=0;
+            Console.Write("Producto: ");
+            producto = Console.ReadLine();
+            Console.Write("Precio: ");
+            precio = double.Parse(Console.ReadLine());
+            Console.Write("Cantidad: ");
+            cantidad = int.Parse(Console.ReadLine());
+
+            fact.Producto = producto;
+            fact.Precio = precio;
+            fact.Cantidad = cantidad;
+
+            monto = fact.calcularMonto();
+
+            Console.WriteLine($"{producto}");
+            Console.WriteLine($"{precio}");
+            Console.WriteLine($"{cantidad}");
+            Console.WriteLine($"{monto}");
+        }
+    }
+}
---
+++ SEMANA 1/Nota.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularNota.clases
+{
+    internal class Nota
+    {
+        public int Cantidad { get; set; }
+    }
+}
