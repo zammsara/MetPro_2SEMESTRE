57fdedf752b265e42046680d1d09f416b7821bdd
zammsara <saztaylor@uamv.edu.ni>
2024-09-02 08:51:35 -0600

    02 SEP

---
+++ SEMANA 3/UseIf/UseIf/Program.cs
@@ -0,0 +1,30 @@
+﻿using UseIf.models;
+
+namespace UseIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Decidir si es niño, joven o adulto
+             
+            Niño: 0-12
+            Joven: 13-25
+            Adulto:26 a ++
+            */
+
+            int edad = 0;
+            Console.WriteLine("Evaluar edad");
+            Console.WriteLine("Ingresa tu edad: ");
+            try
+            {
+                edad = int.Parse(Console.ReadLine());
+                Evaluate evaluate = new Evaluate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
